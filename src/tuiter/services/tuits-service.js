c6af6b09c8bdda713001b6acc0bfaf2e0c5bcfcf
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-08 12:15:53 -0400

    1

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
 // const API_BASE = process.env.REACT_APP_API_BASE;
 // const API_BASE = "https://tuiter-node-server-app-hqru.onrender.com/api";
-const TUITS_API = "https://tuiter-node-server-app-hqru.onrender.com/api";
+const TUITS_API = "https://tuiter-node-server-app-hqru.onrender.com/api/tuits";
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
