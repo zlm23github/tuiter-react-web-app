501444057f6ae5e9224dec3829a4c094adfc1bdf
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-08 11:27:38 -0400

    6

--- src/tuiter/services/tuits-thunks.js
+++ src/tuiter/services/tuits-thunks.js
@@ -3,14 +3,12 @@ import * as service from "./tuits-service";
 
 
 export const findTuitsThunk = createAsyncThunk(
-//  "tuits/findTuits",
-"findTuits",
+ "tuits/findTuits",
  async () => await service.findTuits()
 );
 
 export const deleteTuitThunk = createAsyncThunk(
-    // 'tuits/deleteTuit',
-    'deleteTuit',
+    'tuits/deleteTuit',
     async (tuitId) => {
       await service.deleteTuit(tuitId)
       return tuitId
@@ -18,16 +16,14 @@ export const deleteTuitThunk = createAsyncThunk(
 )
 
 export const createTuitThunk = createAsyncThunk(
-    // 'tuits/createTuit',
-    'createTuit',
+    'tuits/createTuit',
     async (tuit) => {
       const newTuit = await service.createTuit(tuit)
       return newTuit
   })
 
 export const updateTuitThunk = createAsyncThunk(
-    // 'tuits/updateTuit',
-    'updateTuit',
+    'tuits/updateTuit',
     async (tuit) =>
       await service.updateTuit(tuit)
 )
