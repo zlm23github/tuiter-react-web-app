ce104bcc69f49367baa9d9774072e9eba7b6e0b3
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-08 14:23:18 -0400

    1

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -12,6 +12,7 @@ function ProfileScreen() {
  useEffect(() => {
    const loadProfile = async () => {
      const { payload } = await dispatch(profileThunk());
+     console.log(108);
      console.log(payload);
      setProfile(payload);
    };
