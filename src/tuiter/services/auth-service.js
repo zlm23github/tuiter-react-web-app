ad14e548af4e2ac3a09d0863c4c10d9be0975dfa
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-14 13:50:49 -0400

    1

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const SERVER_API_URL = "http://localhost:4000/api";
 // const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 // const SERVER_API_URL = "https://tuiter-node-server-app-hqru.onrender.com/api";
-const USERS_URL = `${process.env.SERVER_API_URL}`;
+const USERS_URL = `${process.env.SERVER_API_URL}/users`;
 
 const api = axios.create({ withCredentials: true });
 
