4efd23f7d65e9474ed37ddf2b56315e07fb323b9
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-15 14:54:28 -0400

    1

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
+import { useSelector, useDispatch, useEffect} from "react-redux";
+import { useNavigate} from "react-router";
 import { profileThunk, logoutThunk, updateUserThunk } from "../services/auth-thunks";
 
 function ProfileScreen() {
@@ -8,13 +8,12 @@ function ProfileScreen() {
  const [ profile, setProfile ] = useState(currentUser);
  const dispatch = useDispatch();
  const navigate = useNavigate();
- 
 
  const save = async () => { await dispatch(updateUserThunk(profile)); };
 
  useEffect(() => {
-    const loadProfile =  () => {
-      const { payload } = dispatch(profileThunk());
+    const loadProfile = async () => {
+      const { payload } = await dispatch(profileThunk());
       console.log(payload);
       setProfile(payload);
     };
