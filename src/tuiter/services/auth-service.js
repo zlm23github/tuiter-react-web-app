995c6163653edc0909629f394f2998ddc9c52351
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-15 14:30:34 -0400

    1

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -24,8 +24,15 @@ export const profile = async () => {
     const response = await api.post(`${USERS_URL}/profile`);
     return response;
 };
+
+// export const updateUser = async (user) => {
+//     const response = await api.put(`${USERS_URL}/${user._id}`, user);
+//     return response.data;
+// };
+
+
 export const updateUser = async (user) => {
-    const response = await api.put(`${USERS_URL}/${user._id}`, user);
+    const response = await api.put(`${USERS_URL}`, user);
     return response.data;
 };
 export const register = async ({username, password}) => {
