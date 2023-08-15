617f3cdcae76e592da3aa2bb0d38246df844850f
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-15 14:32:45 -0400

    1

--- src/tuiter/services/auth-thunks.js
+++ src/tuiter/services/auth-thunks.js
@@ -9,16 +9,16 @@ export const loginThunk = createAsyncThunk(
  }
 );
 
-// export const profileThunk = createAsyncThunk(
-//     "auth/profile", async () => {
-//     const response = await authService.profile();
-//     return response.data;
-// });
-
 export const profileThunk = createAsyncThunk(
     "auth/profile", async () => {
-    return await authService.profile();
+    const response = await authService.profile();
+    return response.data;
 });
+
+// export const profileThunk = createAsyncThunk(
+//     "auth/profile", async () => {
+//     return await authService.profile();
+// });
 
 export const logoutThunk = createAsyncThunk(
     "auth/logout", async () => {
@@ -31,18 +31,18 @@ export const updateUserThunk = createAsyncThunk(
         return user;
 });
 
-// export const registerThunk = createAsyncThunk(
-//     "auth/register", async(user) => {
-//         const newUser = await authService.register(user);
-//         return newUser;
-//     }
-// )
-
-
 export const registerThunk = createAsyncThunk(
-    "auth/register", async(credentials) => {
-        const newUser = await authService.register(credentials);
+    "auth/register", async(user) => {
+        const newUser = await authService.register(user);
         return newUser;
     }
 )
+
+
+// export const registerThunk = createAsyncThunk(
+//     "auth/register", async(credentials) => {
+//         const newUser = await authService.register(credentials);
+//         return newUser;
+//     }
+// )
    
